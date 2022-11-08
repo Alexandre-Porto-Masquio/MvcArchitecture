64cfe8a81d4534633cdc72b37c13265c641f0be5
alexandre.porto <alexandre.porto@wansoft.com.br>
2022-11-08 14:56:41 -0300

    Mvc working with ListView, before changing to RecyclerView

---
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListAdapter.java
@@ -0,0 +1,76 @@
+package com.techyourchance.mvc.screens.questionslist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.techyourchance.mvc.R;
+import com.techyourchance.mvc.questions.Question;
+
+public class QuestionsListAdapter extends ArrayAdapter<Question> implements QuestionsListItemViewMvc.Listener {
+
+    private final OnQuestionClickListener mOnQuestionClickListener;
+
+    public interface OnQuestionClickListener {
+        void onQuestionClicked(Question question);
+    }
+
+    public QuestionsListAdapter(Context context,
+                                OnQuestionClickListener onQuestionClickListener) {
+        super(context, 0);
+        mOnQuestionClickListener = onQuestionClickListener;
+    }
+
+    private static class ViewHolder {
+        private TextView mTxtTitle;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+//            convertView = LayoutInflater.from(parent.getContext())
+//                    .inflate(R.layout.layout_question_list_item, parent, false);
+//            ViewHolder viewHolder = new ViewHolder();
+//            viewHolder.mTxtTitle = convertView.findViewById(R.id.txt_title);
+//            convertView.setTag(viewHolder);
+
+            QuestionsListItemViewMvc viewMvc = new QuestionsListItemViewMvcImpl(
+                    LayoutInflater.from(getContext()),
+                    parent
+            );
+            viewMvc.registerListener(this);
+
+            convertView = viewMvc.getRootView();
+            convertView.setTag(viewMvc);
+        }
+
+        final Question question = getItem(position);
+
+        // bind the data to views
+//        TextView txtTitle = convertView.findViewById(R.id.txt_title);
+//        txtTitle.setText(question.getTitle());
+        QuestionsListItemViewMvc viewMvc = (QuestionsListItemViewMvc) convertView.getTag();
+        viewMvc.bindQuestion(question);
+
+//        // set listener
+//        convertView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onQuestionClicked(question);
+//            }
+//        });
+
+        return convertView;
+    }
+
+    @Override
+    public void onQuestionClicked(Question question) {
+        mOnQuestionClickListener.onQuestionClicked(question);
+    }
+}
